be9cb3b8917ae6635d4ef72209f121bdaf28e117
DanielGermanosilva <ddger2018@gmail.com>
2023-02-09 19:56:27 -0300

    Update Program.cs

--- Gestao/ConsoleAppPrincipal/Program.cs
+++ Gestao/ConsoleAppPrincipal/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+
+
+
+    private static void Main(string[] args)
+    {
+        int x;
+        int y;
+        int z;
+
+        while (true)
+        {
+
+
+            y = Convert.ToInt32(Console.ReadLine());
+            z = Convert.ToInt32(Console.ReadLine());
+           
+
+            try
+            {
+                x = y / z;
+                Console.WriteLine(x);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu um erro a tentar realizar a divisao");
+            }
+            
+        }
+    }
+
+
+}
