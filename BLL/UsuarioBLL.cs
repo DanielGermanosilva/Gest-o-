a419a91ac44c1a4512e0dfb4d2fadaa54406f784
DanielGermanosilva <ddger2018@gmail.com>
2023-02-13 20:56:24 -0300

    Update UsuarioBLL.cs

--- BLL/UsuarioBLL.cs
+++ BLL/UsuarioBLL.cs
@@ -5,7 +5,17 @@ namespace BLL
     {
         public void Inserir(Usuario _usuario)
         {
+            if (_usuario,NomeUsuario.Lenght <= 3){
+                throw new Exception("Nome de Usuario deve ter mais de 3 caracteres");
 
+                // todo: validar se a senha nao é 1234
+                if (_usuario.Senha == "1234")
+                {
+                    throw new Exception("A senha nao pode ser 1234");
+                }
+                // todo: validar se ja existe um usuario com este nome.
+
+            }
         }
 
         public Usuario Buscar(string _nomeUsuario)
