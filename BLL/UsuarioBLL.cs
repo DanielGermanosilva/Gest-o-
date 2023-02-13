166ae4c99da5f7096c14c9b57c593894f94c8be7
DanielGermanosilva <ddger2018@gmail.com>
2023-02-13 20:00:09 -0300

    Update UsuarioBLL.cs

--- BLL/UsuarioBLL.cs
+++ BLL/UsuarioBLL.cs
@@ -1,7 +1,28 @@
-﻿namespace BLL
+﻿using Models
 {
     public class UsuarioBLL
     {
+        public void Inserir(Usuario _usuario)
+        {
+
+        }
+
+        public Usuario Buscar(string _nomeUsuario)
+        {
+
+        }
+        public void Alterar(Usuario _usuario)
+        {
+
+        }
+        public void Excluir(int)
+        {
+
+        }
+
+
+
+
 
     }
 }
