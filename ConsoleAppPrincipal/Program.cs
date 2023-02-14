c3c3b821849b004b15443554d4d916f4c1eced8f
DanielGermanosilva <ddger2018@gmail.com>
2023-02-13 21:52:10 -0300

    Update Program.cs

--- ConsoleAppPrincipal/Program.cs
+++ ConsoleAppPrincipal/Program.cs
@@ -1,33 +1,17 @@
-﻿public class Program
+﻿using BLL;
+public class Program
 {
 
 
 
     private static void Main(string[] args)
     {
-        int x;
-        int y;
-        int z;
 
-        while (true)
-        {
+        UsuarioBLL usuarioBll = new UsuarioBLL();
 
 
-            y = Convert.ToInt32(Console.ReadLine());
-            z = Convert.ToInt32(Console.ReadLine());
-           
 
-            try
-            {
-                x = y / z;
-                Console.WriteLine(x);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ocorreu um erro a tentar realizar a divisao  " + ex.Message);
-            }
-            
-        }
+
     }
 
 
