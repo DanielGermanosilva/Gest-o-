db3dc9e3571c8b9c20ad922e54cfced7374a4d8f
DanielGermanosilva <ddger2018@gmail.com>
2023-02-14 19:56:23 -0300

    Update Program.cs

--- ConsoleAppPrincipal/Program.cs
+++ ConsoleAppPrincipal/Program.cs
@@ -8,18 +8,23 @@ public class Program
 
     private static void Main(string[] args)
     {
+        try
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nome = "Daniel Germano";
+            usuario.NomeUsuario = "DJ";
+            usuario.Ativo = true;
+            usuario.Email = "ddger2018@gmail.com";
+            usuario.CPF = "458.158.442-88";
+            usuario.Senha = "102030";
 
-        UsuarioBLL usuarioBll = new UsuarioBLL();
-        Usuario usuario = new Usuario();
-
-
-        usuario.Nome = "Daniel Germano";
-        usuario.NomeUsuario = "DJ";
-        usuario.Ativo = true;
-        usuario.Email = "ddger2018@gmail.com";
-        usuario.CPF = "458.158.442-88";
-        usuario.Senha = "102030";
-        usuarioBll.Inserir(usuario);
+            UsuarioBLL usuarioBll = new UsuarioBLL();
+            usuarioBll.Inserir(usuario);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
     }
 
 
