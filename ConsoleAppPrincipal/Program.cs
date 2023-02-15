9338d670874f43774fefe8eb38646bea2bee4b26
DanielGermanosilva <ddger2018@gmail.com>
2023-02-14 21:41:12 -0300

    Update Program.cs

--- ConsoleAppPrincipal/Program.cs
+++ ConsoleAppPrincipal/Program.cs
@@ -6,32 +6,49 @@ using System.Security.Cryptography.X509Certificates;
 
 public class Program
 {
-    
+
 
 
     private static void Main(string[] args)
     {
-        
+
         try
         {
             Usuario usuario = new Usuario();
 
             UsuarioBLL usuarioBll = new UsuarioBLL();
-            
+
+            string opc;
             do
             {
+                Console.WriteLine("Digite Seu Nome: ");
+                usuario.Nome = Console.ReadLine();
+                //  usuario.Nome = "Daniel Germano";
 
-                usuario.Nome = "Daniel Germano";
-                usuario.NomeUsuario = "DJ5555";
+                Console.WriteLine("Crie um Nome de Usuario: ");
+                usuario.NomeUsuario = Console.ReadLine();
+                // usuario.NomeUsuario = "DJ5555";
+
+                Console.WriteLine("Digite Seu Email: ");
+                usuario.Email = Console.ReadLine();
+                //  usuario.Email = "ddger2018@gmail.com";
+
+                Console.WriteLine("Digite Seu CPF: ");
+                usuario.CPF = Console.ReadLine();
+                //  usuario.CPF = "458.158.442-88";
+
+                Console.WriteLine("Crie Uma Senha: ");
+                usuario.Senha = Console.ReadLine();
+                //   usuario.Senha = "10203055555";
+
                 usuario.Ativo = true;
-                usuario.Email = "ddger2018@gmail.com";
-                usuario.CPF = "458.158.442-88";
-                usuario.Senha = "10203055555";
-
 
                 usuarioBll.Inserir(usuario);
+                Console.WriteLine("\n\nUsuario Registrado Com Sucesso!!!\n\n\n" + "Deseja Criar Mais Um Usuario? ");
+                opc = Console.ReadLine();
+                Console.Clear();
 
-            } while (true);
+            } while (opc == "sim");
 
 
         }
