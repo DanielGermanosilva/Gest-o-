56bb0116d43c0397b786b349d4d9851e2e7e7d74
DanielGermanosilva <ddger2018@gmail.com>
2023-03-02 19:46:11 -0300

    up

--- Configuração/WindowsFormsAppPrincipal/Gestao.Designer.cs
+++ Configuração/WindowsFormsAppPrincipal/Gestao.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsAppPrincipal
 {
-    partial class Form1
+    partial class Gestao
     {
         /// <summary>
         /// Required designer variable.
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1103, 628);
+            this.ClientSize = new System.Drawing.Size(1306, 628);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Configuração/WindowsFormsAppPrincipal/Gestao.cs
+++ Configuração/WindowsFormsAppPrincipal/Gestao.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace WindowsFormsAppPrincipal
 {
-    public partial class Form1 : Form
+    public partial class Gestao : Form
     {
-        public Form1()
+        public Gestao()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
